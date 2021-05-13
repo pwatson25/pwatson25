75230d2be86c48b46f2e0f5e7ce81497219d117c
peterjwatson <peterskyguy@gmail.com>
2021-05-13 21:37:20 +0200

    nav changes

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -1,3 +1,11 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.menu')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 468) mainnav.classList.remove('responsive')};
+
 const thisYear = new Date();
 let year = thisYear.getFullYear();
 document.getElementById("currentYear").textContent = year;
