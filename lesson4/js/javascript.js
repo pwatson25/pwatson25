930a9d6961cc0423e0670cee236b65ee951b9e97
peterjwatson <peterskyguy@gmail.com>
2021-05-13 17:39:24 +0200

    master

---
+++ lesson4/js/javascript.js
@@ -0,0 +1,9 @@
+const thisYear = new Date();
+let year = thisYear.getFullYear();
+document.getElementById("currentYear").textContent = year;
+
+const lastModDate = new Date(document.lastModified);
+let strLMD = + lastModDate.getMonth()+1 + "/" + lastModDate.getDate() + "/" 
+           + lastModDate.getFullYear() + " " + lastModDate.getHours() + ":" 
+           + lastModDate.getMinutes() + ":" + lastModDate.getSeconds();
+document.getElementById("lastUpdate").textContent = strLMD;
