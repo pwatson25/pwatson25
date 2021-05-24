a72a0d92884b4c21a4cfcaf247337c9ece0b8b55
peterjwatson <peterskyguy@gmail.com>
2021-05-24 13:55:14 +0200

    BOM fixes

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,16 +1,19 @@
-const input = document.querySelector("#fav");
+const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const output = document.querySelector(".list");
 
 button.addEventListener('click', () => {
     if (input.value != '' ) {
         let li = document.createElement('li');
+        li.textContent = input.value;
+
         let deletebutton = document.createElement
         ('button');
-        li.textContent = input.value;
         deletebutton.textContent = '❌';
+
         li.append(deletebutton);
         output.append(li);
+        
         deletebutton.addEventListener('click', function ()
         {
             output.removeChild(li);
