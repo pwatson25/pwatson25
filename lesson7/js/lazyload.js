711072bf8663293d5e138e0288e77c4f38307f78
peterjwatson <peterskyguy@gmail.com>
2021-06-03 09:54:37 +0200

    lazyload fix (typo in js)

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,30 +1,31 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
 
 const imgOptions = {
     threshold: 1,
     rootMargin: "0px 0px 50px 0px",
 };
 
-const loadImages = (Image) => {
-    Image.setAtribute("src", Image.getAttribute("data-src"));
-    Image.onload = () => {
-        Image.removeAttribute("data-src");
-    };
-};
-
-if ("IntersectionObserver" in window) {
-    const imgObserver = new IntersectionObserver ((items, imgObserver) => {
-        items.forEach((item) => {
-            if (item.isInterseting) {
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
                 loadImages(item.target);
                 imgObserver.unobserve(item.target);
             }
         });
     }, imgOptions);
 
-    imagesToLoad.forEach((img) => {
+    imagesToLoad.forEach(img => {
         imgObserver.observe(img);
     });
+    
 } else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
