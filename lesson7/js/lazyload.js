946259ac85b4229f967f9d1c978da1c44ac6c820
peterjwatson <peterskyguy@gmail.com>
2021-06-03 09:16:53 +0200

    big changes bigger messes

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,32 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px",
+};
+
+const loadImages = (Image) => {
+    Image.setAtribute("src", Image.getAttribute("data-src"));
+    Image.onload = () => {
+        Image.removeAttribute("data-src");
+    };
+};
+
+if ("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver ((items, imgObserver) => {
+        items.forEach((item) => {
+            if (item.isInterseting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
