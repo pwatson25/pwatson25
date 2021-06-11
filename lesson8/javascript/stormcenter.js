7b5cd21759dda5a9ca63e907b3a2c11aed8c183f
peterjwatson <peterskyguy@gmail.com>
2021-06-11 18:14:29 +0200

    form correction (now functional)

--- lesson8/javascript/stormcenter.js
+++ lesson8/javascript/stormcenter.js
@@ -35,7 +35,7 @@ function nextPrev(n) {
   // if you have reached the end of the form...
   if (currentTab >= x.length) {
     // ... the form gets submitted:
-    document.getElementById("regForm").submit();
+    document.getElementById("weatherForm").submit();
     return false;
   }
   // Otherwise, display the correct tab:
