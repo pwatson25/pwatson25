81f75cd076cdf3b8fbf7e7511dc2e039ff18bd8e
peterjwatson <peterskyguy@gmail.com>
2021-06-19 19:41:53 +0200

    master

--- lesson9/js/towndata.js
+++ lesson9/js/towndata.js
@@ -21,18 +21,24 @@ fetch(requestURL)
         let p = document.createElement('p');
         let p2 = document.createElement('p');
         let p3 = document.createElement('p');
+        let button = document.createElement('button');
 
         h2.textContent = `${towns.name}`;
         h3.textContent = `${towns.motto}`;
         p.textContent = `Year Founded: ${towns.yearFounded}`;
         p2.textContent = `Current Population: ${towns.yearFounded}`;
         p3.textContent = `Average Rainfall: ${towns.averageRainfall}`;
+        button.innerHTML = "Visit...";
+        button.setAttribute('onclick', `location.href='${towns.name}.html'`);
+        button.setAttribute('type', 'button');
+
 
         div.appendChild(h2);
         div.appendChild(h3);
         div.appendChild(p);
         div.appendChild(p2);
         div.appendChild(p3);
+        div.appendChild(button);
 
          
         let img = document.createElement('img');
