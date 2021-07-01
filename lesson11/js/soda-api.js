bf4041f1be45eb6603078738394f3512087b1016
peterjwatson <peterskyguy@gmail.com>
2021-07-01 13:08:16 +0200

    keep saving

--- lesson11/js/soda-api.js
+++ lesson11/js/soda-api.js
@@ -1,5 +1,5 @@
   
-fetch('https://api.openweathermap.org/data/2.5/weather?&id=5604473&units=imperial&APPID=98cbc04c44e9fc2905a70710f2643e4e')
+fetch('https://api.openweathermap.org/data/2.5/weather?&id=5607916&units=imperial&APPID=98cbc04c44e9fc2905a70710f2643e4e')
 .then((response) => response.json())
 .then((jsObject) => {
     console.log(jsObject);
@@ -63,23 +63,7 @@ const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=560
         }
 
     });
-    fetch('https://api.nasa.gov/planetary/apod?api_key=kJMweCft7Sek5sPPm4bQ68PqvHPZACO8gMoKXTJu')
-.then((response) => response.json())
-.then((jsObject) => {
-    console.log(jsObject);
-    
-        let title = jsObject.title;
-        let explanation = jsObject.explanation;
-    
-        document.getElementById('title').textContent = (title);
-        document.getElementById('explanation').textContent = (explanation);
 
-        const imagesrc = jsObject.url;
-        const desc = jsObject.title;
-        document.getElementById('photo').setAttribute('src', imagesrc);
-        document.getElementById('photo').setAttribute('alt', desc);
-    
-    });
 
 
 
