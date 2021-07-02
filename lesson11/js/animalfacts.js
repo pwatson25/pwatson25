6a4aeb5382fd3432637b1a18e4a3554b48d3bfa2
peterjwatson <peterskyguy@gmail.com>
2021-07-02 13:54:42 +0200

    animals functioning

---
+++ lesson11/js/animalfacts.js
@@ -0,0 +1,70 @@
+fetch('https://some-random-api.ml/animal/panda')
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    
+        let panda = jsObject.fact;
+    
+        document.getElementById('panda').textContent = (panda);
+
+        const imagesrc = jsObject.image;
+        document.getElementById('pandaPhoto').setAttribute('src', imagesrc);
+        document.getElementById('pandaPhoto').setAttribute('alt', "Panda Photo");
+    
+    });
+fetch('https://some-random-api.ml/animal/racoon')
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    
+        let racoon = jsObject.fact;
+    
+        document.getElementById('racoon').textContent = (racoon);
+
+        const src = jsObject.image;
+        document.getElementById('racoonPhoto').setAttribute('src', src);
+        document.getElementById('racoonPhoto').setAttribute('alt', "Racoon Photo");
+    
+    });
+    fetch('https://some-random-api.ml/animal/kangaroo')
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    
+        let kangaroo = jsObject.fact;
+    
+        document.getElementById('kangaroo').textContent = (kangaroo);
+
+        const src = jsObject.image;
+        document.getElementById('kangarooPhoto').setAttribute('src', src);
+        document.getElementById('kangarooPhoto').setAttribute('alt', "Kangaroo Photo");
+    
+    });
+    fetch('https://some-random-api.ml/animal/koala')
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    
+        let koala = jsObject.fact;
+    
+        document.getElementById('koala').textContent = (koala);
+
+        const src = jsObject.image;
+        document.getElementById('koalaPhoto').setAttribute('src', src);
+        document.getElementById('koalaPhoto').setAttribute('alt', "Koala Photo");
+    
+    });
+    fetch('https://some-random-api.ml/animal/fox')
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        
+            let fox = jsObject.fact;
+        
+            document.getElementById('fox').textContent = (fox);
+    
+            const src = jsObject.image;
+            document.getElementById('foxPhoto').setAttribute('src', src);
+            document.getElementById('foxPhoto').setAttribute('alt', "Fox Photo");
+        
+        });
