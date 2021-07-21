23fb1365a68ed8d899e2044249e4258a680ac488
peterjwatson <peterskyguy@gmail.com>
2021-07-21 22:40:12 +0200

    master

--- final/greater-bowie/javascript/business.js
+++ final/greater-bowie/javascript/business.js
@@ -7,10 +7,10 @@ fetch(requestURL)
   .then(function (jsonObject) {
     const business = jsonObject['business'];
     console.log(business);
-    
+
     const company = document.querySelector('.company');
 
-    const top3 = business.filter(x => x.companyName == 'National Capital Radio and Televison Museum' || x.companyName == 'Patuxent Nursery' || x.companyName == 'SMaRT Fitness');
+    const top3 = business.filter(x => x.companyName == 'Sky Zone Trampoline Park' || x.companyName == 'Patuxent Nursery' || x.companyName == 'SMaRT Fitness');
     console.log(top3)
 
     top3.forEach(business => {
@@ -23,31 +23,107 @@ fetch(requestURL)
       let info = document.createElement('div');
       info.setAttribute('class', 'info');
 
-      let img = document.createElement('img');
-      img.setAttribute('src', `https://storage.googleapis.com/pwatson25_photos/${towns.photo}`);
-      img.setAttribute('alt', `${towns.name}`);
-      img.setAttribute('loading', 'lazy');
-
       let h2 = document.createElement('h2')
       let button = document.createElement('button');
 
-      h2.textContent = `${buisness.companyName}`;
+      h2.textContent = `${business.companyName}`;
       button.innerHTML = "Visit...";
-      button.setAttribute('onclick', `location.href="${buisness.url}"`);
+      button.setAttribute('onclick', `location.href="${business.url}"`);
       button.setAttribute('type', 'button');
       button.setAttribute('class', 'visit');
 
-      info.appendChild(img);
       info.appendChild(h2);
       info.appendChild(button);
 
-      info.appendChild(div);
-      top.appendChild(div);
-
-      // let webicon = document.createElement('div');
-      // webicon.setAttribute('class', 'webicon');
+      top.appendChild(info);
+      back.appendChild(top);
 
 
+      let webicon = document.createElement('div');
+      webicon.setAttribute('class', 'webicon');
+
+      let div = document.createElement('div');
+
+      let contact = document.createElement('div');
+      contact.setAttribute('class', 'phone');
+
+      let phone = document.createElement('img');
+
+      phone.setAttribute('src', './images/call.svg');
+      phone.setAttribute('class', 'icon')
+      phone.setAttribute('alt', `phone icon`);
+      phone.setAttribute('loading', 'lazy');
+    
+      let phoneNumber = document.createElement('div');
+      let number = document. createElement('p');
+
+      number.textContent = `${business.phoneNumber}`;
+      
+      phoneNumber.appendChild(number);
+      contact.appendChild(phoneNumber);
+      contact.appendChild(phone);
+      div.appendChild(contact);
+
+      let hours = document.createElement('div');
+      hours.setAttribute('class', 'hours');
+
+      let div2 = document.createElement('div');      
+
+      let clock = document.createElement('img');
+
+      clock.setAttribute('src', './images/clock.svg');
+      clock.setAttribute('class', 'icon')
+      clock.setAttribute('alt', `phone icon`);
+      clock.setAttribute('loading', 'lazy');
+    
+      let open = document.createElement('div');
+      let monFri = document.createElement('p');
+      let sat = document.createElement('p');
+      let sun = document.createElement('p');
+
+      monFri.textContent = `Mon - Fri: ${business.hours.monFri}`;
+      sat.textContent = `Sat: ${business.hours.sat}`;
+      sun.textContent = `Sun: ${business.hours.sun}`;
+      
+      open.appendChild(monFri);
+      open.appendChild(sat);
+      open.appendChild(sun);
+      hours.appendChild(open);
+      hours.appendChild(clock);
+      div2.appendChild(hours);
+
+      //new
+
+      let location = document.createElement('div');
+      location.setAttribute('class', 'location');
+
+      let div3 = document.createElement('div');      
+
+      let pin = document.createElement('img');
+
+      pin.setAttribute('src', './images/pin.svg');
+      pin.setAttribute('class', 'icon')
+      pin.setAttribute('alt', `phone icon`);
+      pin.setAttribute('loading', 'lazy');
+    
+      let address = document.createElement('div');
+      let streetAddress = document.createElement('p');
+
+      streetAddress.textContent = `1234 Big Drive`;
+      
+
+      address.appendChild(streetAddress);
+      location.appendChild(address);
+      location.appendChild(pin);
+      div3.appendChild(location);
+
+      //end
+
+      webicon.appendChild(contact);
+      webicon.appendChild(hours);
+      webicon.appendChild(location);
+
+      back.appendChild(webicon);
       // let p2 = document.createElement('p');
 
       // h3.textContent = `${buisness.address.streetAddress}`;
---
+++ final/greater-bowie/javascript/weather.js
@@ -0,0 +1,50 @@
+  
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=38.9428&lon=-76.7303&exclude=hourly,minutely&units=imperial&APPID=98cbc04c44e9fc2905a70710f2643e4e')
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+
+    // let temp = jsObject.current.temp;
+    // let minTemp = jsObject.daily[0].temp.min;
+    // let maxTemp = jsObject.daily[0].temp.max;
+
+    // document.getElementById('condition').textContent = jsObject.current.weather[0].main;
+    // document.getElementById('temp-min').textContent = Math.round(minTemp);
+    // document.getElementById('temp-max').textContent = Math.round(maxTemp);
+    // document.getElementById('temp-current').textContent = Math.round(temp);
+
+        
+        var weekday = new Array(7);
+        weekday[0] = "Sun";
+        weekday[1] = "Mon";
+        weekday[2] = "Tue";
+        weekday[3] = "Wed";
+        weekday[4] = "Thu";
+        weekday[5] = "Fri";
+        weekday[6] = "Sat";
+
+        var data = jsObject.daily;
+        var dayOfWeek = document.getElementsByClassName("forecast-day");
+        var weatherIcon = document.getElementsByClassName("weatherIcon");
+        var tempMax = document.getElementsByClassName("forecast-max");
+        var tempMin = document.getElementsByClassName("forecast-min");
+
+        for (var i = 0; i < data.length; i++) {
+            var d = new Date(data[i].dt);
+            dayOfWeek[i].textContent = weekday[d.getDay()+i];
+            const imagesrc = 'https://openweathermap.org/img/w/' + data[i].weather[0].icon + '.png';
+            const description = data[i].weather[0].description;
+            
+            weatherIcon[i].setAttribute('src', imagesrc);
+            weatherIcon[i].setAttribute('alt', description);
+
+            tempMax[i].innerHTML = Math.round(data[i].temp.max) + " &#176;F";
+            tempMin[i].innerHTML = Math.round(data[i].temp.min) + " &#176;F";
+
+        }
+
+    });
+
+
+
+
